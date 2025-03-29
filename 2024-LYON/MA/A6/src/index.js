c0f686be6570d14df7c15598841d6c7be1c17fec
AmirparsaDD <Amirparsab9@gmail.com>
2025-03-29 17:09:40 +0330

    Fix MA-A6

--- 2024-LYON/MA/A6/src/index.js
+++ 2024-LYON/MA/A6/src/index.js
@@ -1,8 +1,8 @@
-// window.addEventListener("load", () => { // When the page has loaded
-// 	const loading = document.querySelector("#loading"); // Select the loading overlay using its ID
-// 	loading.style.opacity = 0; // Set the opacity to 0 (to slowly hide it with a transition)
+window.addEventListener("load", () => { // When the page has loaded
+	const loading = document.querySelector("#loading"); // Select the loading overlay using its ID
+	loading.style.opacity = 0; // Set the opacity to 0 (to slowly hide it with a transition)
 
-// 	setTimeout(() => { // After 1s
-// 		loading.style.display = "none"; // Hide it to allow events to be triggered on elements behind it
-// 	}, 1000) // 1s is the transition duration set in the CSS
-// })
+	setTimeout(() => { // After 1s
+		loading.style.display = "none"; // Hide it to allow events to be triggered on elements behind it
+	}, 1000) // 1s is the transition duration set in the CSS
+})
