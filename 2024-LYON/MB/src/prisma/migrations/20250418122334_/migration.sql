34196e3511763140a32bad64aadee4c66d5fa47a
AmirparsaDD <Amirparsab9@gmail.com>
2025-04-18 16:05:32 +0330

    squash migrations

--- 2024-LYON/MB/src/prisma/migrations/20250418122334_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Product` DROP FOREIGN KEY `Product_companyID_fkey`;
-
--- DropIndex
-DROP INDEX `Product_companyID_key` ON `Product`;
-
--- AddForeignKey
-ALTER TABLE `Product` ADD CONSTRAINT `Product_companyID_fkey` FOREIGN KEY (`companyID`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- 2024-LYON/MB/src/prisma/migrations/20250418123502_squashed_migrations/migration.sql
+++ 2024-LYON/MB/src/prisma/migrations/20250418123502_squashed_migrations/migration.sql
@@ -15,6 +15,7 @@ CREATE TABLE `Company` (
     `address` VARCHAR(191) NOT NULL,
     `tel` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `deactivated` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `Company_id_key`(`id`),
     PRIMARY KEY (`id`)
@@ -46,8 +47,32 @@ CREATE TABLE `Contact` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name_EN` VARCHAR(191) NOT NULL,
+    `name_FR` VARCHAR(191) NOT NULL,
+    `gtin` VARCHAR(191) NOT NULL,
+    `brand` VARCHAR(191) NOT NULL,
+    `country` VARCHAR(191) NOT NULL,
+    `grossWeight` DOUBLE NOT NULL,
+    `netContentWeight` DOUBLE NOT NULL,
+    `weightUnit` VARCHAR(191) NOT NULL,
+    `desc_EN` LONGTEXT NOT NULL,
+    `desc_FR` LONGTEXT NOT NULL,
+    `hidden` BOOLEAN NOT NULL DEFAULT false,
+    `companyID` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Product_id_key`(`id`),
+    UNIQUE INDEX `Product_gtin_key`(`gtin`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Owner` ADD CONSTRAINT `Owner_companyID_fkey` FOREIGN KEY (`companyID`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Contact` ADD CONSTRAINT `Contact_companyID_fkey` FOREIGN KEY (`companyID`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_companyID_fkey` FOREIGN KEY (`companyID`) REFERENCES `Company`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
